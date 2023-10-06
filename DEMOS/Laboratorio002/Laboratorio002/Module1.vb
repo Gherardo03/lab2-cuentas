29329e806553f0c0076c9d9990227af44fa5c97a
Gherardo03 <47503498+Gherardo03@users.noreply.github.com>
2023-10-05 18:41:49 -0600

    Add files via upload

---
+++ DEMOS/Laboratorio002/Laboratorio002/Module1.vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim totalCompra As Double = 0
+
+        While True
+            Console.WriteLine("Ingrese el costo del artículo vendido (o un número negativo para salir): ")
+            Dim costo As Double = Convert.ToDouble(Console.ReadLine())
+
+            If costo < 0 Then
+                Exit While
+            End If
+
+            Console.WriteLine("Ingrese la cantidad de dinero entregada por el cliente: ")
+            Dim dineroEntregado As Double = Convert.ToDouble(Console.ReadLine())
+
+            If costo > 500 Then
+                costo *= 0.95 ' Aplicar un 5% de descuento si el costo supera los $500
+            ElseIf costo > 1000 Then
+                costo *= 0.9 ' Aplicar un 10% de descuento si el costo supera los $1000
+            End If
+
+            If dineroEntregado > costo Then
+                Dim cambio As Double = dineroEntregado - costo
+                Console.WriteLine("Cambio a entregar: $" & cambio)
+            ElseIf dineroEntregado = costo Then
+                Console.WriteLine("Entrega del producto.")
+            Else
+                Console.WriteLine("El cliente debe pagar $" & (costo - dineroEntregado) & " más para completar la compra.")
+                Console.WriteLine("El cliente no posee suficiente dinero. Vuelva pronto por el producto que desea.
+                    Recibido por Mauricio Alberto Mendoza Y Gerardo Alexander Cabrera")
+            End If
+
+            totalCompra += costo
+        End While
+
+        Console.WriteLine("Total de la venta: $" & totalCompra)
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ DEMOS/Laboratorio2.00/Laboratorio2.00/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+            Dim costo As Double
+            Dim dineroEntregado As Double
+            Dim cambio As Double
+        Dim totalCompra As Double = 0
+
+        Console.WriteLine("Ingrese el costo del artículo: ")
+            costo = Double.Parse(Console.ReadLine())
+
+            Console.WriteLine("Ingrese la cantidad de dinero entregada por el cliente: ")
+            dineroEntregado = Double.Parse(Console.ReadLine())
+
+        If dineroEntregado > costo Then
+            cambio = dineroEntregado - costo
+            Console.WriteLine("Cambio a entregar: $" & cambio)
+        ElseIf dineroEntregado = costo Then
+            Console.WriteLine("Entrega del producto realizada. Gracias por su compra lo esperamos nuevamente.")
+
+        Else
+            Console.WriteLine("El cliente debe pagar $" & (costo - dineroEntregado) & " adicional para completar el pago.")
+            Console.WriteLine("¿Desea pagar la cantidad restante? (S/N)")
+            Dim opcion As Char = Char.Parse(Console.ReadLine())
+            If opcion = "S" Or opcion = "s" Then
+                Console.WriteLine("Entrega del producto realizada. Gracias por su compra.")
+            Else
+                Console.WriteLine("El cliente no posee suficiente dinero. Vuelva pronto por el producto que desea.")
+            End If
+        End If
+
+        Console.WriteLine("Atendio Mauricio Alberto Y Gerardo Cabrera")
+
+        Console.ReadLine()
+        End Sub
+    End Module
+
+
+
+
+
+
+
+
